c52b6b8ffbfa0510bfbaaee9ed317261ebe0790d
steve <steve@5a6c10e1-6920-0410-8c7b-9669c677a970>
2008-03-25 16:19:45 +0000

    Reverted changes from R144-R145 for the graphics bug as it just made things worse

--- TransX/trunk/basefunction.cpp
+++ TransX/trunk/basefunction.cpp
@@ -821,6 +821,12 @@ void basefunction::doupdate(HDC hDC,int tw, int th,int viewmode)
 	}
 	else
 	{
+		//Draw the central body
+		SelectDefaultPen(hDC,PEN_ATMOSPHERE);
+		graph.drawatmosphere(hDC,hmajor); 
+		SelectDefaultPen(hDC,Grey);
+		graph.drawplanet(hDC,hmajor); 
+
 		VECTOR3 ntemp={0,-1,0};
 		graph.setviewwindow(0,0,tw,th);
 		graph.setprojection(ntemp);
@@ -841,11 +847,6 @@ void basefunction::doupdate(HDC hDC,int tw, int th,int viewmode)
 			if (!planpointer->maingraph(hDC,&graph,this)) 
 			{
 				planpointer->wordupdate(hDC,tw,th,this);
-				//Draw the central body
-				SelectDefaultPen(hDC,PEN_ATMOSPHERE);
-				graph.drawatmosphere(hDC,hmajor); 
-				SelectDefaultPen(hDC,Grey);
-				graph.drawplanet(hDC,hmajor); 
 				return;
 			}
 		}
@@ -866,12 +867,6 @@ void basefunction::doupdate(HDC hDC,int tw, int th,int viewmode)
 			}
 			graph.setviewscale(rmin);
 		}
-		//Draw the central body
-		SelectDefaultPen(hDC,PEN_ATMOSPHERE);
-		graph.drawatmosphere(hDC,hmajor); 
-		SelectDefaultPen(hDC,Grey);
-		graph.drawplanet(hDC,hmajor); 
-
 		if (target.isvalid() && m_scale!=2) graph.setviewscale(target);
 		if (hypormaj.isvalid() && m_scale==0) graph.setviewscale(hypormaj);
 		if (planpointer!=NULL && m_scale==0)
